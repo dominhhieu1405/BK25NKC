b6fd6627711d6937ade8e90f3dc694b0150ae040
Luce <51281387+dominhhieu1405@users.noreply.github.com>
2023-09-08 23:14:52 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+Date.prototype.getWeek = function () {
+    var date = new Date(this.getTime());
+    date.setHours(0, 0, 0, 0);
+    date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    return 1 + Math.round(((date - week1) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7);
+};
+const daysOfWeek = ["Chủ Nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy"];
+const dOW = ["CN", "T2", "T3", "T4", "T5", "T6", "T7"];
